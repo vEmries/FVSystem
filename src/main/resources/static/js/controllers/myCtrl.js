b5665b5d28e49ce038c123b9047f28cf32c86914
Username <mail@mail.com>
2017-10-31 19:52:03 +0100

    https://path fix

--- src/main/resources/static/js/controllers/myCtrl.js
+++ src/main/resources/static/js/controllers/myCtrl.js
@@ -3,7 +3,7 @@ app.controller('myCtrl', function($scope, $http){
     $scope.fv = [];
     
     $scope.loadFV = function() {
-        $http.get("https://localhost:8080/fv")
+        $http.get("https://fv-system.herokuapp.com/fv")
                 .success(function(data) {
                     $scope.fv = data;
         });
