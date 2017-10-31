9b562ecb5da9661e4b9f6ad9f269072a14d9aa72
Username <mail@mail.com>
2017-10-31 12:17:58 +0100

    Working https://

--- src/main/resources/static/js/controllers/myCtrl.js
+++ src/main/resources/static/js/controllers/myCtrl.js
@@ -3,7 +3,7 @@ app.controller('myCtrl', function($scope, $http){
     $scope.fv = [];
     
     $scope.loadFV = function() {
-        $http.get("http://localhost:8080/fv")
+        $http.get("https://localhost:8080/fv")
                 .success(function(data) {
                     $scope.fv = data;
         });
