edadc8864e108a71fa26faac9bcf50751a5194ef
Username <mail@mail.com>
2017-11-02 23:51:00 +0100

    Fixed https:// path in myCtrl

--- src/main/resources/static/js/controllers/myCtrl.js
+++ src/main/resources/static/js/controllers/myCtrl.js
@@ -3,7 +3,7 @@ app.controller('myCtrl', function($scope, $http){
     $scope.fv = [];
     
     $scope.loadFV = function() {
-        $http.get("https://fv-system.herokuapp.com/fv")
+        $http.get("https://localhost:8080/fv")
                 .success(function(data) {
                     $scope.fv = data;
         });
